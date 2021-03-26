8dedcc3db2a1deca2676b781d832a10858de45b1
Pavel Kiselyov <pavel@blockchain.com>
2021-03-26 23:24:45 +0000

    Initial Commit

---
+++ cryptoboy/cryptoboyApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoboyApp.swift
+//  cryptoboy
+//
+//  Created by Pavel on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct cryptoboyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
