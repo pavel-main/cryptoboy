1c4df290a0ca7d8350b2c555636a57dac4c47255
Pavel Kiselyov <pavel@blockchain.com>
2021-03-27 03:10:22 +0000

    icon

--- cryptoboy/Extensions.swift
+++ cryptoboy/Extensions.swift
@@ -10,7 +10,23 @@ import WalletCore
 
 extension String {
 
+    func hex() -> String {
+        return Data(self.utf8).hexString
+    }
+    
+    func sha1() -> String {
+        return Hash.sha1(data: Data(self.utf8)).hexString
+    }
+    
     func sha256() -> String {
         return Hash.sha256(data: Data(self.utf8)).hexString
+    }
+    
+    func keccak256() -> String {
+        return Hash.keccak256(data: Data(self.utf8)).hexString
+    }
+    
+    func ripemd160() -> String {
+        return Hash.ripemd(data: Data(self.utf8)).hexString
     }
 }
