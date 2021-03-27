bfb27fcb598235b6220c04c398934e85095eccaa
Pavel Kiselyov <pavel@blockchain.com>
2021-03-27 00:14:14 +0000

    Navigation menu

--- cryptoboy/ContentView.swift
+++ cryptoboy/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(menu, id: \.id) { section in
+                    Section(header: Text(section.name)) {
+                        ForEach(section.items, id: \.id) { item in
+                            ItemRow(item: item)
+                        }
+                    }
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Menu")
+        }
     }
 }
 
