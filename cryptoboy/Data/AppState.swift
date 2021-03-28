c1cddbf9bf5613650c06007b6f4283c6a5a29a03
Pavel Kiselyov <pavel@blockchain.com>
2021-03-28 02:36:01 +0100

    Move to appstate

--- cryptoboy/Data/AppState.swift
+++ cryptoboy/Data/AppState.swift
@@ -24,4 +24,54 @@ class AppState : ObservableObject {
         self.hashMessage = ""
         self.hashMessageChanged = false
     }
+    
+    func getDigest(_ type: String, _ placeholder: String) -> String {
+        if (hashMessage.isEmpty && !hashMessageChanged) {
+            return placeholder
+        }
+        
+        switch type {
+        case "sha1":
+            return hashMessage.sha1()
+            
+        case "sha256":
+            return hashMessage.sha256()
+        
+        case "sha256ripedm160":
+            return hashMessage.sha256ripemd160()
+
+        case "sha256sha256":
+            return hashMessage.sha256sha256()
+            
+        case "sha512":
+            return hashMessage.sha512()
+            
+        case "keccak256":
+            return hashMessage.keccak256()
+
+        case "keccak512":
+            return hashMessage.keccak512()
+            
+        case "ripemd160":
+            return hashMessage.ripemd160()
+            
+        case "blake256":
+            return hashMessage.blake256()
+        
+        case "blake256ripedm160":
+            return hashMessage.blake256ripemd160()
+
+        case "blake256blake256":
+            return hashMessage.blake256blake256()
+            
+        case "groestl512":
+            return hashMessage.groestl512()
+            
+        case "groestl512groestl512":
+            return hashMessage.groestl512groestl512()
+
+        default:
+            return "Invalid hash function \(type)"
+        }
+    }
 }
--- cryptoboy/Views/Functions/HashingItemView.swift
+++ cryptoboy/Views/Functions/HashingItemView.swift
@@ -17,6 +17,7 @@ struct HashingItemView: View {
     init(_ type: String, _ placeholder: String) {
         self.type = type
         self.placeholder = placeholder
+        // self.digest = state.getDigest(type, placeholder)
     }
     
     var body: some View {
@@ -26,66 +27,16 @@ struct HashingItemView: View {
                 TextField(self.placeholder, text: $digest)
                     .disabled(true)
                     .onChange(of: state.hashMessage) { newValue in
-                        digest = getDigest(self.state)
+                        digest = state.getDigest(type, placeholder)
                     }
                 Button(action: {
-                    UIPasteboard.general.string = getDigest(self.state)
+                    UIPasteboard.general.string = state.getDigest(type, placeholder)
                 }) {
                     Image(systemName: "doc.on.doc")
                 }
             }
         }
         .environmentObject(state)
-    }
-    
-    func getDigest(_ state: AppState) -> String {
-        if (state.hashMessage.isEmpty && !state.hashMessageChanged) {
-            return placeholder
-        }
-        
-        switch self.type {
-        case "sha1":
-            return state.hashMessage.sha1()
-            
-        case "sha256":
-            return state.hashMessage.sha256()
-        
-        case "sha256ripedm160":
-            return state.hashMessage.sha256ripemd160()
-
-        case "sha256sha256":
-            return state.hashMessage.sha256sha256()
-            
-        case "sha512":
-            return state.hashMessage.sha512()
-            
-        case "keccak256":
-            return state.hashMessage.keccak256()
-
-        case "keccak512":
-            return state.hashMessage.keccak512()
-            
-        case "ripemd160":
-            return state.hashMessage.ripemd160()
-            
-        case "blake256":
-            return state.hashMessage.blake256()
-        
-        case "blake256ripedm160":
-            return state.hashMessage.blake256ripemd160()
-
-        case "blake256blake256":
-            return state.hashMessage.blake256blake256()
-            
-        case "groestl512":
-            return state.hashMessage.groestl512()
-            
-        case "groestl512groestl512":
-            return state.hashMessage.groestl512groestl512()
-
-        default:
-            return "Invalid hash function \(type)"
-        }
     }
 }
 
