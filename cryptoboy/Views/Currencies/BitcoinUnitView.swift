fb5989001da26597c2425f12ca06472131499010
Pavel <kpashka@gmail.com>
2021-04-01 17:30:24 +0100

    Unit converter generalized (#1)
    * update unit converter
    
    * save progress
    
    * save progress
    
    * save progress
    
    * save progress
    
    * save progress
    
    * RC
    
    * RC
    
    * RC
    
    Co-authored-by: Pavel Kiselyov <pavel@blockchain.com>

--- cryptoboy/Views/Currencies/BitcoinUnitView.swift
+++ cryptoboy/Views/Currencies/BitcoinUnitView.swift
@@ -10,51 +10,30 @@ import SwiftUI
 struct BitcoinUnitView: View {
     @EnvironmentObject var state: AppState
     
+    static let MAX_FACTOR = 8
+    
+    @State var units = [
+        MinorUnit.init(title: "Bitcoin", factor: 8, max: MAX_FACTOR),
+        MinorUnit.init(title: "milliBTC", factor: 5, max: MAX_FACTOR),
+        MinorUnit.init(title: "microBTC", factor: 2, max: MAX_FACTOR),
+        MinorUnit.init(title: "Satoshi", factor: 0, max: MAX_FACTOR),
+    ]
+    
     var body: some View {
         Form {
-            Section(header: Text("Bitcoin")) {
-                TextField("", text: Binding<String>(
-                    get: { CryptoFormatter.bitcoin.string(for: state.amount)! },
-                    set: {
-                        if let value = CryptoFormatter.bitcoin.number(from: $0) {
-                            state.amount = value.decimalValue
-                        }
-                    }
-                ))
-                .keyboardType(.decimalPad)
-            }
-            Section(header: Text("mBTC")) {
-                TextField("", text: Binding<String>(
-                    get: { CryptoFormatter.milliBTC.string(for: state.amount * 1_000)! },
-                    set: {
-                        if let value = CryptoFormatter.milliBTC.number(from: $0) {
-                            state.amount = value.decimalValue / 1_000
-                        }
-                    }
-                ))
-                .keyboardType(.decimalPad)
-            }
-            Section(header: Text("microBTC")) {
-                TextField("", text: Binding<String>(
-                    get: { CryptoFormatter.microBTC.string(for: state.amount * 1_000_000)! },
-                    set: {
-                        if let value = CryptoFormatter.microBTC.number(from: $0) {
-                            state.amount = value.decimalValue / 1_000_000
-                        }
-                    }
-                ))
-                .keyboardType(.decimalPad)
-            }
-            Section(header: Text("Satoshi")) {
-                TextField("", text: Binding<String>(
-                    get: { CryptoFormatter.satoshi.string(for: state.amount * 100_000_000)! },
-                    set: {
-                        if let value = CryptoFormatter.satoshi.number(from: $0) {
-                            state.amount = value.decimalValue / 100_000_000
-                        }
-                    }
-                ))
-                .keyboardType(.decimalPad)
+            ForEach(units.indices, id: \.self) { idx in
+                Section(header: Text(self.units[idx].title)) {
+                    TextField("", text: Binding(
+                        get: {
+                            return self.units[idx].current
+                        },
+                        set: { (newValue) in
+                            let updated = CryptoUnitFormatter.updateUnits(self.units, idx, newValue)
+                            self.units = updated
+                        })
+                    )
+                    .keyboardType(.decimalPad)
+                }
             }
         }
         .modifier(NavigationViewModifier(page: .btc_unit))
